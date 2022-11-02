752ff146c83d35bb79b608a294d30315e6b59625
jose esteves <josefelixestevenpacheco@gmail.com>
2022-11-02 01:13:25 -0400

    feat: :art: schemas and routes
    the user's schema and route file was created

--- src/config/express.js
+++ src/config/express.js
@@ -1,4 +1,8 @@
-import express from 'express'
-const app = express()
-// TODO poner lo middleware y routers
-export default app
+import userRouter from '#Routes/user.routes.js';
+import express from 'express';
+const app = express();
+// middleware
+app.use(express.json());
+// routers
+app.use('/user', userRouter);
+export default app;
